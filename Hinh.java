085d0a9ea33edcc21cd9d5c708f628d3853edba5
Elysia0211 <124846387+Elysia0211@users.noreply.github.com>
2023-03-23 08:57:47 +0700

    Create Hinh.java

---
+++ Hinh.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hinh;
+
+/**
+ *
+ * @author Admin
+ */
+public class Hinh {
+    protected String loaiHinh(){
+        return null;
+    }
+   protected double DienTich(){
+       
+        return 0;
+       
+   }
+
+    public Hinh() {
+    }
+   public void xuatTHongTin(){
+       System.out.println("loai Hinh:"+loaiHinh());
+       System.out.println("dien tich: "+DienTich());
+       
+   }
+
+}
